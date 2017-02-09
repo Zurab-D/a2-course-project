5ec894f26ddedd3b71a07410df1429f7158bc703
Zurab-D <dzn@inbox.com>
2017-02-10 01:02:09 +0300

    config file changed

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,14 +1,14 @@
 ﻿export let CONFIG = {
     /*urls: {
-        users: "http://test-api.javascript.ru/v1/lexa/users",
-        letters: "http://test-api.javascript.ru/v1/lexa/letters",
-        mailboxes: "http://test-api.javascript.ru/v1/lexa/mailboxes"
+        users: "https://test-api.javascript.ru/v1/lexa/users",
+        letters: "https://test-api.javascript.ru/v1/lexa/letters",
+        mailboxes: "https://test-api.javascript.ru/v1/lexa/mailboxes"
     }*/
     urls: {
-        root: "http://test-api.javascript.ru/v1/zurab/",
-        users: "http://test-api.javascript.ru/v1/zurab/users",
-        letters: "http://test-api.javascript.ru/v1/zurab/letters",
-        mailboxes: "http://test-api.javascript.ru/v1/zurab/mailboxes"
+        root: "https://test-api.javascript.ru/v1/zurab/",
+        users: "https://test-api.javascript.ru/v1/zurab/users",
+        letters: "https://test-api.javascript.ru/v1/zurab/letters",
+        mailboxes: "https://test-api.javascript.ru/v1/zurab/mailboxes"
     },
     userMailField: "email"
 }
